233479116bf895500e7dbd49ecfb20c7afc0ecaf
Juan Carlos Kerchaten <jcfiles07@gmail.com>
2026-05-13 11:12:19 +0900

    chore: add countries seed data

---
+++ src/data/countries.ts
@@ -0,0 +1,8 @@
+import type { Country } from "@/types/emissions";
+
+export const countries: Country[] = [
+  { code: "US", name: "United States" },
+  { code: "DE", name: "Germany" },
+  { code: "KR", name: "South Korea" },
+  { code: "PH", name: "Philippines" },
+];
